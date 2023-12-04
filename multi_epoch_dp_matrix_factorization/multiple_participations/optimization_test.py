2152acf45d23957a7b4fbb2dc0ec719556a209c2
Yingtai Xiao <1025965245@qq.com>
2023-12-04 15:17:44 -0500

    Update optimization_test.py

--- multi_epoch_dp_matrix_factorization/multiple_participations/optimization_test.py
+++ multi_epoch_dp_matrix_factorization/multiple_participations/optimization_test.py
@@ -102,7 +102,7 @@ class OptimizationTest(tf.test.TestCase):
   def test_solves_simple_problem(self):
     key = random.PRNGKey(0)  # You can use any integer seed here
     gap = 0.001
-    n = 8
+    n = 10
    
     a = np.arange(n, 0, -1)
     r = np.random.random(n)
